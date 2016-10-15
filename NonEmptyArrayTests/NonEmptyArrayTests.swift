fe6d051ca0ab3b7178c3db8bb92c52a9beb18536
Soroush Khanlou <soroushkhanlou@gmail.com>
2016-10-15 19:53:34 -0400

    check non-optional methods return non-optionals

--- NonEmptyArrayTests/NonEmptyArrayTests.swift
+++ NonEmptyArrayTests/NonEmptyArrayTests.swift
@@ -14,13 +14,29 @@ class NonEmptyArrayTests: XCTestCase {
     
     let nilArray = NonEmptyArray(array: [])
 
-    let array = NonEmptyArray(array: [1, 2, 3])
+    let notNilArray = NonEmptyArray(array: [1, 2, 3])
+    
+    let array = NonEmptyArray(elements: 1, 2, 3)
 
     func testEmptyArrayInitializer() {
         XCTAssertNil(nilArray)
     }
     
     func testArray() {
-        XCTAssertEqual(array?.count, 3)
+        XCTAssertEqual(array.count, 3)
+    }
+
+    func testNonOptionalMethods() {
+        XCTAssert(type(of: array.min()) == Int.self)
+        XCTAssert(type(of: array.min()) != Optional<Int>.self)
+
+        XCTAssert(type(of: array.max()) == Int.self)
+        XCTAssert(type(of: array.max()) != Optional<Int>.self)
+
+        XCTAssert(type(of: array.first) == Int.self)
+        XCTAssert(type(of: array.first) != Optional<Int>.self)
+
+        XCTAssert(type(of: array.last) == Int.self)
+        XCTAssert(type(of: array.last) != Optional<Int>.self)
     }
 }

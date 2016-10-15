3d1815325a2871c29df448bc8e6e5b59b43f2133
Soroush Khanlou <soroushkhanlou@gmail.com>
2016-10-15 19:38:15 -0400

    add Xcode project and first cut of the NonEmptyArray

---
+++ NonEmptyArray/NonEmptyArray.h
@@ -0,0 +1,19 @@
+//
+//  NonEmptyArray.h
+//  NonEmptyArray
+//
+//  Created by Soroush Khanlou on 10/15/16.
+//  Copyright © 2016 Soroush Khanlou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NonEmptyArray.
+FOUNDATION_EXPORT double NonEmptyArrayVersionNumber;
+
+//! Project version string for NonEmptyArray.
+FOUNDATION_EXPORT const unsigned char NonEmptyArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NonEmptyArray/PublicHeader.h>
+
+
---
+++ NonEmptyArray/NonEmptyArray.swift
@@ -0,0 +1,89 @@
+//
+//  NonEmptyArray.swift
+//  NonEmptyArray
+//
+//  Created by Soroush Khanlou on 10/15/16.
+//  Copyright © 2016 Soroush Khanlou. All rights reserved.
+//
+
+import Foundation
+
+public struct NonEmptyArray<Element> {
+    
+    fileprivate var elements: Array<Element>
+    
+    public init?(array: [Element]) {
+        guard array.count != 0 else {
+            return nil
+        }
+        self.elements = array
+    }
+    
+    public var count: Int {
+        return elements.count
+    }
+    
+    public var first: Element {
+        return elements.first!
+    }
+    
+    public var last: Element {
+        return elements.last!
+    }
+    
+    public var isEmpty: Bool {
+        return false
+    }
+    
+    public mutating func append(_ newElement: Element) {
+        elements.append(newElement)
+    }
+    
+    public func appending(_ newElement: Element) -> NonEmptyArray<Element> {
+        var copy = self
+        copy.append(newElement)
+        return copy
+    }
+}
+
+extension NonEmptyArray: CustomStringConvertible {
+    public var description: String {
+        return elements.description
+    }
+}
+
+extension NonEmptyArray: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        return elements.debugDescription
+    }
+}
+
+extension NonEmptyArray: Collection {
+    public typealias Index = Int
+    
+    public var startIndex: Int {
+        return 0
+    }
+    
+    public var endIndex: Int {
+        return count
+    }
+    
+    public subscript(_ index: Int) -> Element {
+        return elements[index]
+    }
+    
+    public func index(after i: Int) -> Int {
+        return i + 1
+    }
+}
+
+extension NonEmptyArray where Element: Comparable {
+    public func min() -> Element {
+        return elements.min()!
+    }
+    
+    public func max() -> Element {
+        return elements.max()!
+    }
+}
